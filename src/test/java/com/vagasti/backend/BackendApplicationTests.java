62ebcb18cdcf7c9eb20150726764af4863e0dc12
vitorgongora <41486681+vitorgongora@users.noreply.github.com>
2023-01-02 21:45:33 -0300

    test gh actions fail

--- src/test/java/com/vagasti/backend/BackendApplicationTests.java
+++ src/test/java/com/vagasti/backend/BackendApplicationTests.java
@@ -3,9 +3,16 @@ package com.vagasti.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.fail;
+
 @SpringBootTest
 class BackendApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  @Test
+  void forceFail() {
+    fail("Expected exception was not thrown");
+  }
 }
